ca0a891dbe371382254403a4d36f01e7329184ca
Eka Awaludin <ekaawaludin@outlook.com>
2021-04-02 14:59:14 +0700

    Update typewriter.js

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,11 +1,11 @@
-let wrapperx;
+let wrapper;
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 async function writingAll(stringTarget, container) {
   wrapper = document.querySelector('[' + container + ']');
   const stringsContainer = document.getElementsByClassName(stringTarget);
 
-  while (wrapperx) {if (window.CP.shouldStopExecution(0)) break;
+  while (wrapper) {if (window.CP.shouldStopExecution(0)) break;
     for (i = 0; i < stringsContainer.length; i++) {if (window.CP.shouldStopExecution(1)) break;
       const string = stringsContainer[i].textContent;
       await write(string);
@@ -22,17 +22,17 @@ async function write(text) {
     const timeout = 100;
     await sleep(timeout);
     index++;
-    wrapperx.innerHTML = text.substring(0, index);
+    wrapper.innerHTML = text.substring(0, index);
   }window.CP.exitedLoop(2);
 };
 
 
 async function erase() {
-  while (wrapperx.textContent.length) {if (window.CP.shouldStopExecution(3)) break;
+  while (wrapper.textContent.length) {if (window.CP.shouldStopExecution(3)) break;
     const timeout = 100;
     await sleep(timeout);
-    wrapperx.textContent = wrapperx.textContent.substring(0, wrapperx.textContent.length - 2);
+    wrapper.textContent = wrapper.textContent.substring(0, wrapper.textContent.length - 2);
   }window.CP.exitedLoop(3);
 };
 
-writingAll('itemx', 'data-text');
+writingAll('item', 'data-text');
